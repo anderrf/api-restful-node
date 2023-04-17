ff936ed250033672ced2eca1147da65689d5822e
anderrf <anderson_rocha_farias@outlook.com>
2023-04-17 16:40:46 -0300

    Addition of routes for transactions' creation and listing

---
+++ README.md
@@ -0,0 +1,14 @@
+# FR - Functional Requirements
+
+- [X] User should be able to create a new transaction
+- [X] User should be able to get a summary of its account
+- [X] User should be able to list all the previous transactions
+- [X] User should be able to see a single transaction
+
+# BN - Business Rules
+
+- [X] Transaction should be of types credit, wich will increase the total value, or debit, wich will decrease it
+- [ ] It should be able to identify the user between requests
+- [ ] User can only see transactions made by them
+
+# NR - Nonfunctional Requirements
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,13 @@
 import { env } from './env'
 import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'node:crypto'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: randomUUID(),
-      title: 'Test',
-      amount: 1000,
-    })
-    .returning('*')
-  console.log(transaction)
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-  return transactions
+app.register(transactionRoutes, {
+  prefix: 'transactions',
 })
+
 app
   .listen({
     port: env.PORT,
