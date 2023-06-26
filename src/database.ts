5f95d61630650cc61dd45b67c500b6df85ec2776
anderrf <anderson_rocha_farias@outlook.com>
2023-06-26 18:34:46 -0300

    Addition of pg (PostgreSQL) client

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,16 @@
 import Knex from 'knex'
 import { env } from './env'
 
+const databaseUrl =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
+
 export const config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection: databaseUrl,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
